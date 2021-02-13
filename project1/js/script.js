7498ae248783c670236690b6a775852de54f850b
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-02-13 00:05:33 -0500

    P1: began project 1, converted the first two verses of the murder mystery into json

---
+++ project1/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+The Murder Mystery
+Quinn Zwarich
+
+A visualization of The Murder Mystery, written by The Velvet Underground.
+It is not only meant to be an intrepetation of the song but as well a tool for understanding the dense lyricism.
+*/
+
+
+
+function preload() {
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}

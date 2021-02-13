a85cb78c5c2d11184470abf1752f54895a472c82
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-02-13 18:47:50 -0500

    P1: made minor changes

--- project1/js/script.js
+++ project1/js/script.js
@@ -8,16 +8,19 @@ A visualization of The Murder Mystery, written by The Velvet Underground.
 It is not only meant to be an intrepetation of the song but as well a tool for understanding the dense lyricism.
 */
 
-
+let lyricData;
 
 function preload() {
-
+  lyricData = loadJSON(`assets/data/lyrics.json`);
 }
 
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  let verse = lyricData.verse1_0.morrison;
+  console.log(verse.join(` `));
 }
 
 function draw() {
-
+  background(255);
 }

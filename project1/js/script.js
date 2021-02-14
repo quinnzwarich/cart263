b489d674ab445a9d539e6aae45038cfac083f25c
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-02-14 00:03:35 -0500

    P1: found a solution to the rate problem, still determining how to play multiple voices at the same time

--- project1/js/script.js
+++ project1/js/script.js
@@ -16,11 +16,41 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  let verse = lyricData.verse1_0.morrison;
-  console.log(verse.join(` `));
 }
 
 function draw() {
   background(255);
+}
+
+// function assessRate() {
+//   let morrison = lyricData.verse1_0.morrison;
+//   let reed = lyricData.verse1_0.reed;
+//
+//   let mPercent = 100 / morrison.elements;
+//   let rPercent = 100 / reed.elements;
+//
+//   let ratio = mPercent / rPercent;
+//   return ratio;
+// }
+
+function recitedMorrison() {
+  let morrison = lyricData.verse1_0.morrison;
+
+  responsiveVoice.speak(morrison.join(`  `), "UK English Male", {onstart: reciteReed});
+}
+
+function reciteReed() {
+  let morrison = lyricData.verse1_0.morrison;
+  let reed = lyricData.verse1_0.reed;
+
+  let mPercent = 100 / morrison.length;
+  let rPercent = 100 / reed.length;
+
+  let ratio = mPercent / rPercent;
+
+  responsiveVoice.speak(reed.join(`  `), "UK English Male", {rate: ratio});
+}
+
+function mousePressed() {
+  recitedMorrison();
 }

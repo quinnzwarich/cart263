f41d923fc2b135823aea8bc7d286416db99b9b7b
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-02-24 01:02:37 -0500

    P1: fixed the JSON file, developed a better means of sequencing the data

--- project1/js/script.js
+++ project1/js/script.js
@@ -9,14 +9,25 @@ It is not only meant to be an intrepetation of the song but as well a tool for u
 */
 
 let lyricData;
+
+let search = ``;
+let url = ``;
+
 let findLyric = {
-  verse: `verse0_0`,
-  numVerse: 0,
-  subVerse: 0,
+  verse: 0,
+  stanza: 0,
   index: 0
 };
-let images = [];
-let words = [];
+let morrison = {
+  rate: 0,
+  images: [],
+  words: []
+};
+let reed = {
+  rate: 0,
+  images: [],
+  words: []
+};
 
 function preload() {
   lyricData = loadJSON(`assets/data/lyrics.json`);
@@ -24,67 +35,112 @@ function preload() {
 
 function setup() {
   noCanvas();
-  setInterval(morrison, 2000);
+  assessRate();
+  setInterval(updateMorrison, morrison.rate);
 }
 
 function assessRate() {
-  let morrison = lyricData.verse1_0.morrison;
-  let reed = lyricData.verse1_0.reed;
+  let mLength = lyricData.verses[findLyric.verse][findLyric.stanza].morrison.length;
+  let rLength = lyricData.verses[findLyric.verse][findLyric.stanza].reed.length;
 
-  let mPercent = 100 / morrison.elements;
-  let rPercent = 100 / reed.elements;
-
+  let mPercent = 100 / mLength;
+  let rPercent = 100 / rLength;
   let ratio = mPercent / rPercent;
-  return ratio;
+
+  morrison.rate = 500;
+  reed.rate = morrison.rate * ratio;
 }
 
 function sequenceMorrison() {
-  findLyric.index++;
-  if (findLyric.index ===
-  lyricData.verse0_0.morrison.length) {
+  if (
+    findLyric.index <=
+    lyricData.verses[findLyric.verse][findLyric.stanza].morrison.length
+  ) {
+    findLyric.index++;
+  } else if (
+    findLyric.index >
+    lyricData.verses[findLyric.verse][findLyric.stanza].morrison.length
+  ) {
+    findLyric.stanza++;
     findLyric.index = 0;
+    assessRate();
+    if (findLyric.stanza > lyricData.verses[findLyric.verse].length * 2) {
+      findLyric.verse++;
+      assessRate();
+      if (findLyric.verse > lyricData.verses.length) {
+        findLyric.index = 0;
+        findLyric.stanza = 0;
+        findLyric.verse = 0;
+      }
+    }
   }
 }
 
+
 function getMorrisonImage() {
-  let search = lyricData.verse0_0.morrison[findLyric.index];
-  let url = `https://loremflickr.com/320/240/${search}`;
-  if (images.length === 0) {
+  search = lyricData.verses[findLyric.verse][findLyric.stanza].morrison[findLyric.index];
+  url = `https://loremflickr.com/320/240/${search}`;
+  if (morrison.images.length === 0) {
     // display the image
-    let img = createImg(url);
-    images.push(img);
-    // display the word
-    let word = createDiv(`${search}`);
-    words.push(word);
+    let img = createImg(url, ``,
+    `anonymous`, displayMorrisonWord);
+    morrison.images.push(img);
   }
   else {
     // remove the previous image
-    let trash = images[0];
+    let trash = morrison.images[0];
     trash.remove();
-    images.shift();
+    morrison.images.shift();
     // remove the previous word
-    let garbage = words[0];
+    let garbage = morrison.words[0];
     garbage.remove();
-    words.shift();
+    morrison.words.shift();
     // display the current image
-    let img = createImg(url);
-    images.push(img);
-    // display the current word
-    let word = createDiv(`${search}`);
-    words.push(word);
+    let img = createImg(url, ``,
+    `anonymous`, displayMorrisonWord);
+    morrison.images.push(img);
   }
 }
 
-function morrison() {
+function displayMorrisonWord() {
+  // display the current word
+  let word = createDiv(`${search}`);
+  morrison.words.push(word);
+}
+
+function getReedImage() {
+  let search = lyricData.verses[findLyric.verse][findLyric.stanza].reed[findLyric.index];
+  let url = `https://loremflickr.com/320/240/${search}`;
+  if (reed.images.length === 0) {
+    // display the image
+    let img = createImg(url);
+    reed.images.push(img);
+    // display the word
+    let word = createDiv(`${search}`);
+    reed.words.push(word);
+  }
+  else {
+    // remove the previous image
+    let trash = reed.images[0];
+    trash.remove();
+    reed.images.shift();
+    // remove the previous word
+    let garbage = reed.words[0];
+    garbage.remove();
+    reed.words.shift();
+    // display the current image
+    let img = createImg(url);
+    reed.images.push(img);
+    // display the current word
+    let word = createDiv(`${search}`);
+    reed.words.push(word);
+  }
+}
+
+function updateMorrison() {
   sequenceMorrison();
   getMorrisonImage();
 }
-
-// function getReedImage(verse, subVerse, index) {
-//   let search = lyricData.`verse${verse}_${subVerse}`.reed[index];
-//   let url = `https://loremflickr.com/320/240/${search}`;
-//   let img = createImg(url);
-// }
 
 function mousePressed() {
 

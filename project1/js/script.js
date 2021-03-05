3ecab0db30af137afa819e3aa05cdb871a37f72c
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-03-05 17:36:28 -0500

    P1: displays both images and texts without error

--- project1/js/script.js
+++ project1/js/script.js
@@ -10,24 +10,25 @@ It is not only meant to be an intrepetation of the song but as well a tool for u
 
 let lyricData;
 
-let findLyric = {
-  verse: 0,
-  stanza: 0,
-  index: 0
-};
 let morrison = {
   rate: 4000,
+  callback: false,
   images: [],
   words: [],
-  searches: [],
-  callback: false
+  index: 0,
+  stanza: 0,
+  verse: 0,
+  timer: undefined,
 };
 let reed = {
-  rate: 0,
+  rate: 4000,
+  callback: false,
   images: [],
   words: [],
-  searches: [],
-  callback: false
+  index: 0,
+  stanza: 0,
+  verse: 0,
+  timer:undefined,
 };
 
 function preload() {
@@ -37,12 +38,13 @@ function preload() {
 function setup() {
   noCanvas();
   assessRate();
-  setInterval(updateMorrison, morrison.rate);
+  startMorrison();
+  startReed();
 }
 
 function assessRate() {
-  let mLength = lyricData.verses[findLyric.verse][findLyric.stanza].morrison.length;
-  let rLength = lyricData.verses[findLyric.verse][findLyric.stanza].reed.length;
+  let mLength = lyricData.verses[morrison.verse][morrison.stanza].morrison.length;
+  let rLength = lyricData.verses[reed.verse][reed.stanza].reed.length;
   // compare the length of each vocalists stanza
   let mPercent = 100 / mLength;
   let rPercent = 100 / rLength;
@@ -55,29 +57,53 @@ function assessRate() {
   }
   // lou always speaks fewer words
   // so determine his rate in relation to sterling's
-  reed.rate = morrison.rate * ratio;
+  reed.rate = morrison.rate / ratio;
+}
+
+function startMorrison() {
+  morrison.timer = setInterval(updateMorrison, morrison.rate);
+}
+
+function startReed() {
+  reed.timer = setInterval(updateReed, reed.rate);
 }
 
 function sequenceMorrison() {
-  console.log(findLyric.index);
-  console.log(findLyric.stanza);
-  console.log(findLyric.verse);
-  console.log(morrison.searches);
-
-  if (findLyric.index <
-  lyricData.verses[findLyric.verse][findLyric.stanza].morrison.length &&
+  if (morrison.index <
+  lyricData.verses[morrison.verse][morrison.stanza].morrison.length &&
   morrison.callback) {
-    assessRate();
-    findLyric.index++;
-    if (findLyric.index >=
-    lyricData.verses[findLyric.verse][findLyric.stanza].morrison.length) {
-      findLyric.index = 0;
-      findLyric.stanza++;
-      if (findLyric.stanza >= lyricData.verses[findLyric.verse].length) {
-        findLyric.stanza = 0;
-        findLyric.verse++;
-        if (findLyric.verse > lyricData.verses.length) {
-          findLyric.verse = 0;
+    morrison.index++;
+    if (morrison.index >=
+    lyricData.verses[morrison.verse][morrison.stanza].morrison.length) {
+      // only needs to be called once as it determines both rates
+      assessRate();
+      morrison.index = 0;
+      morrison.stanza++;
+      if (morrison.stanza >= lyricData.verses[morrison.verse].length) {
+        morrison.stanza = 0;
+        morrison.verse++;
+        if (morrison.verse >= lyricData.verses.length) {
+          morrison.verse = 0;
+        }
+      }
+    }
+  }
+}
+
+function sequenceReed() {
+  if (reed.index <
+  lyricData.verses[reed.verse][reed.stanza].reed.length &&
+  reed.callback) {
+    reed.index++;
+    if (reed.index >=
+    lyricData.verses[reed.verse][reed.stanza].reed.length) {
+      reed.index = 0;
+      reed.stanza++;
+      if (reed.stanza >= lyricData.verses[reed.verse].length) {
+        reed.stanza = 0;
+        reed.verse++;
+        if (reed.verse >= lyricData.verses.length) {
+          reed.verse = 0;
         }
       }
     }
@@ -86,49 +112,24 @@ function sequenceMorrison() {
 
 
 function displayMorrison() {
-  if (morrison.searches.length === 0) {
+  if (morrison.images.length === 0) {
     // search for the first image
-    let search = lyricData.verses[findLyric.verse][findLyric.stanza].morrison[findLyric.index];
-    let url = `https://loremflickr.com/320/240/${search}`;
-    morrison.searches.push(search);
+    let search = lyricData.verses[morrison.verse][morrison.stanza].morrison[morrison.index];
+    let url = `https://loremflickr.com/g/480/360/${search}`;
     // cue the sequence
     morrison.callback = true;
     // display the first word
     let word = createDiv(`${search}`);
-    morrison.words.push(word);
+    word.class('morrison'); morrison.words.push(word);
     // display the first image
     let img = createImg(url, ``,
     `anonymous`, morrisonCallback);
-    morrison.images.push(img);
-  }
-  else if (morrison.searches.length === 1) {
-    // find the previous and current search
-    let previousSearch = morrison.searches[0];
-    let search = lyricData.verses[findLyric.verse][findLyric.stanza].morrison[findLyric.index];
-    let url = `https://loremflickr.com/320/240/${previousSearch},${search}/all`;
-    morrison.searches.push(search);
-    // cue the sequence
-    morrison.callback = true;
-    // remove the previous image
-    let trash = morrison.images[0];
-    trash.remove();
-    morrison.images.shift();
-    // display the current word
-    let word = createDiv(`${search}`);
-    morrison.words.push(word);
-    // display the current image
-    let img = createImg(url, ``,
-    `anonymous`, morrisonCallback);
-    morrison.images.push(img);
+    img.class('morrison'); morrison.images.push(img);
   }
   else {
-    // remove the least recent search
-    morrison.searches.shift();
-    // find the previous and current search
-    let previousSearch = morrison.searches[0];
-    let search = lyricData.verses[findLyric.verse][findLyric.stanza].morrison[findLyric.index];
-    let url = `https://loremflickr.com/320/240/${previousSearch},${search}/all`;
-    morrison.searches.push(search);
+    // search for the current image
+    let search = lyricData.verses[morrison.verse][morrison.stanza].morrison[morrison.index];
+    let url = `https://loremflickr.com/g/480/360/${search}`;
     // cue the sequence
     morrison.callback = true;
     // remove the previous image
@@ -137,11 +138,11 @@ function displayMorrison() {
     morrison.images.shift();
     // display the current word
     let word = createDiv(`${search}`);
-    morrison.words.push(word);
+    word.class('morrison'); morrison.words.push(word);
     // display the current image
     let img = createImg(url, ``,
     `anonymous`, morrisonCallback);
-    morrison.images.push(img);
+    img.class('morrison'); morrison.images.push(img);
   }
 }
 
@@ -157,6 +158,55 @@ function morrisonCallback() {
 function updateMorrison() {
   displayMorrison();
   sequenceMorrison();
+}
+
+function displayReed() {
+  if (reed.images.length === 0) {
+    // search for the first image
+    let search = lyricData.verses[reed.verse][reed.stanza].reed[reed.index];
+    let url = `https://loremflickr.com/g/480/360/${search}`;
+    // cue the sequence
+    reed.callback = true;
+    // display the first word
+    let word = createDiv(`${search}`);
+    word.class('reed'); reed.words.push(word);
+    // display the first image
+    let img = createImg(url, ``,
+    `anonymous`, reedCallback);
+    img.class('reed'); reed.images.push(img);
+  }
+  else {
+    // search for the current image
+    let search = lyricData.verses[reed.verse][reed.stanza].reed[reed.index];
+    let url = `https://loremflickr.com/g/480/360/${search}`;
+    // cue the sequence
+    reed.callback = true;
+    // remove the previous image
+    let trash = reed.images[0];
+    trash.remove();
+    reed.images.shift();
+    // display the current word
+    let word = createDiv(`${search}`);
+    word.class('reed'); reed.words.push(word);
+    // display the current image
+    let img = createImg(url, ``,
+    `anonymous`, reedCallback);
+    img.class('reed'); reed.images.push(img);
+  }
+}
+
+function reedCallback() {
+  // remove the previous word
+  let garbage = reed.words[0];
+  garbage.remove();
+  reed.words.shift();
+  // cue the sequence
+  reed.callback = false;
+}
+
+function updateReed() {
+  displayReed();
+  sequenceReed();
 }
 
 function mousePressed() {

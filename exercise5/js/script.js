7871b2c95fd427421c4ceb89db385747a61e98e8
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-03-10 04:07:29 -0500

    E5: made a function which chooses words based on their syllable count

--- exercise5/js/script.js
+++ exercise5/js/script.js
@@ -2,30 +2,18 @@
 haiku generator
 quinn zwarich
 
-generates haikus
+generates completely random haikus using rita.js,
+even the amount of words in each line is random though
+each line still contains the correct number of syllables.
 */
 
 "use strict";
 
-let fiveSyllableLines = [
-  `I keep repeating`,
-  `When you wear those boots`,
-  `In your raining eyes`,
-  `The stars are falling`,
-  `There's bugs in my brain`
-];
+let space = " ";
 
-let sevenSyllableLines = [
-  `Naked in the afternoon`,
-  `Staring at the cellophane`,
-  `Wrestle with a wandering`,
-  `Dying inside a window`,
-  `Walkin' down a lonesome town`
-];
-
-let line1 = random(fiveSyllableLines);
-let line2 = random(sevenSyllableLines);
-let line3 = random(fiveSyllableLines);
+let line1 = generateFiveSyllableLine();
+let line2 = generateSevenSyllableLine();
+let line3 = generateFiveSyllableLine();
 
 let line1P = document.getElementById(`line-1`);
 let line2P = document.getElementById(`line-2`);
@@ -38,6 +26,159 @@ line3P.innerText = line3;
 line1P.addEventListener(`click`, lineClicked);
 line2P.addEventListener(`click`, lineClicked);
 line3P.addEventListener(`click`, lineClicked);
+
+function generateFiveSyllableLine() {
+  // generate first word
+  let firstWord = RiTa.randomWord({
+    numSyllables: getRandomInt(4)
+  }); let firstResult = RiTa.analyze(firstWord);
+  // number of slashes plus one will give syllable count
+  let firstSyllables = firstResult.syllables.split("/").length;
+  // 1st digit is 1
+  if (firstSyllables === 1) {
+    let secondWord = RiTa.randomWord({
+      numSyllables: getRandomInt(4)
+    }); let secondResult = RiTa.analyze(secondWord);
+    let secondSyllables = secondResult.syllables.split("/").length;
+
+    if (secondSyllables === 1) {
+      let thirdWord = RiTa.randomWord({
+        numSyllables: getRandomInt(3)
+      }); let thirdResult = RiTa.analyze(thirdWord);
+      let thirdSyllables = thirdResult.syllables.split("/").length;
+
+      if (thirdSyllables === 1) {
+        let fourthWord = RiTa.randomWord({
+          numSyllables: getRandomInt(2)
+        }); let fourthResult = RiTa.analyze(fourthWord);
+        let fourthSyllables = fourthResult.syllables.split("/").length;
+
+        if (fourthSyllables === 1) {
+          let fifthWord = RiTa.randomWord({
+            numSyllables: 1
+          });
+          return firstWord.concat(space, secondWord, space, thirdWord, space, fourthWord, space, fifthWord);
+        }
+        else if (fourthSyllables === 2) {
+          return firstWord.concat(space, secondWord, space, thirdWord, space, fourthWord);
+        }
+      }
+      else if (thirdSyllables === 2) {
+        let fourthWord = RiTa.randomWord({
+          numSyllables: 1
+        });
+        return firstWord.concat(space, secondWord, space, thirdWord, space, fourthWord);
+      }
+      else if (thirdSyllables === 3) {
+        return firstWord.concat(space, secondWord, space, thirdWord);
+      }
+    } // 2nd digit is 2
+    else if (secondSyllables === 2) {
+      let thirdWord = RiTa.randomWord({
+        numSyllables: getRandomInt(2)
+      }); let thirdResult = RiTa.analyze(thirdWord);
+      let thirdSyllables = thirdResult.syllables.split("/").length;
+
+      if (thirdSyllables === 1) {
+        let fourthWord = RiTa.randomWord({
+          numSyllables: 1
+        });
+        return firstWord.concat(space, secondWord, space, thirdWord, space, fourthWord);
+      }
+      else if (thirdSyllables === 2) {
+        return firstWord.concat(space, secondWord, space, thirdWord);
+      }
+    } // 2nd digit is 3
+    else if (secondSyllables === 3) {
+      let thirdWord = RiTa.randomWord({
+        numSyllables: 1
+      });
+      return firstWord.concat(space, secondWord, space, thirdWord);
+    } // 2nd digit is 4
+    else if (secondSyllables === 4) {
+      return firstWord.concat(space, secondWord);
+    }
+  }
+  // 1st digit is 2
+  if (firstSyllables === 2) {
+    let secondWord = RiTa.randomWord({
+      numSyllables: getRandomInt(3)
+    }); let secondResult = RiTa.analyze(secondWord);
+    let secondSyllables = secondResult.syllables.split("/").length;
+
+    if (secondSyllables === 1) {
+      let thirdWord = RiTa.randomWord({
+        numSyllables: getRandomInt(2)
+      }); let thirdResult = RiTa.analyze(thirdWord);
+      let thirdSyllables = thirdResult.syllables.split("/").length;
+
+      if (thirdSyllables === 1) {
+        let fourthWord = RiTa.randomWord({
+          numSyllables: 1
+        });
+        return firstWord.concat(space, secondWord, space, thirdWord, space, fourthWord);
+      }
+      else if (thirdSyllables === 2) {
+        return firstWord.concat(space, secondWord, space, thirdWord);
+      }
+    } // 2nd digit is 2
+    else if (secondSyllables === 2) {
+      let thirdWord = RiTa.randomWord({
+        numSyllables: 1
+      });
+      return firstWord.concat(space, secondWord, space, thirdWord);
+    }
+    else if (secondSyllables === 3) {
+      firstWord.concat(space, secondWord);
+    }
+  }
+  // 1st digit is 3
+  if (firstSyllables === 3) {
+    let secondWord = RiTa.randomWord({
+      numSyllables: getRandomInt(2)
+    }); let secondResult = RiTa.analyze(secondWord);
+    let secondSyllables = secondResult.syllables.split("/").length;
+
+    if (secondSyllables === 1) {
+      let thirdWord = RiTa.randomWord({
+        numSyllables: 1
+      });
+      return firstWord.concat(space, secondWord, space, thirdWord);
+    }
+    else if (secondSyllables === 2) {
+      return firstWord.concat(space, secondWord);
+    }
+  }
+  // 1st digit is 4
+  if (firstSyllables === 4) {
+    let secondWord = RiTa.randomWord({
+      numSyllables: 1
+    });
+    return firstWord.concat(space, secondWord);
+  }
+}
+
+// after making the five syllable function I got lazy..
+function generateSevenSyllableLine() {
+  // use the previous function for the first words
+  let firstFiveWords = generateFiveSyllableLine();
+  // generate first word
+  let firstWord = RiTa.randomWord({
+    numSyllables: getRandomInt(2)
+  }); let firstResult = RiTa.analyze(firstWord);
+  // number of slashes plus one will give syllable count
+  let firstSyllables = firstResult.syllables.split("/").length;
+  if (firstSyllables === 1) {
+    let secondWord = RiTa.randomWord({
+      numSyllables: 1
+    });
+    let concatenated = firstWord.concat(space, secondWord);
+    return firstFiveWords.concat(space, concatenated);
+  }
+  else if (firstSyllables === 2) {
+    return firstFiveWords.concat(space, firstWord);
+  }
+}
 
 function lineClicked(event) {
   fadeOut(event.target, 1);
@@ -69,14 +210,14 @@ function fadeIn(element, opacity) {
 
 function setNewLine(element) {
   if (element === line1P || element === line3P) {
-    element.innerText = random(fiveSyllableLines);
+    element.innerText = generateFiveSyllableLine();
   }
   else if (element === line2P) {
-    element.innerText = random(sevenSyllableLines);
+    element.innerText = generateSevenSyllableLine();
   }
 }
 
-function random(array) {
-  let index = Math.floor(Math.random() * array.length);
-  return array[index];
+// gripped from mozilla docs
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max - 1) + 1);
 }

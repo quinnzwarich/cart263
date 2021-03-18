317de0dfbf4b11373c6cfe0784f667799aeb95ae
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-03-18 00:59:30 -0400

    finished the activity

--- ravingredactionist/js/script.js
+++ ravingredactionist/js/script.js
@@ -8,4 +8,22 @@ author, and this description to match your project!
 
 "use strict";
 
-// Code goes here
+$(`.top-secret`).on(`click`, redact);
+setInterval(revelation, 500);
+
+function redact(event) {
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
+
+function revelation() {
+  $('.redacted').each(attemptReveal);
+}
+
+function attemptReveal() {
+  let r = Math.random();
+  if (r < 0.1) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}

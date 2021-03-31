1849a1cc590392ca5cc141d5ba5fd3348ef4096f
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-03-31 07:52:39 -0400

    PROTO: completed the model of the pencil

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -5,16 +5,118 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
+let regularHex = [];
+let smallerHex = [];
+let pencilBody = [];
+let radius = 18;
+let segments = 10;
 
+function setup() {
+  createCanvas(500, 500, WEBGL);
+  // end of pencil
+  polygon(0, 0, 0, radius, 6, regularHex);
+  // graphite tip
+  polygon(0, 0, -33, radius/3, 6, smallerHex);
+  // body of pencil ?
+  for (let i = 0; i < segments; i++) {
+    polygon(0, 0, i * 33, radius, 6, pencilBody);
+  }
 }
 
-// draw()
-//
-// Description of draw() goes here.
+
 function draw() {
+  background(250);
+  orbitControl();
+
+  translate(0, height/4, 0);
+  rotateX(PI/2);
+  bottom();
+  middle();
+  eraser();
+}
+
+function polygon(x, y, z, radius, npoints, array) {
+  let angle = 2 * PI / npoints;
+  for (let i = 0; i < 2 * PI; i += angle) {
+    let coordinates = createVector(x + cos(i) * radius, y + sin(i) * radius, z);
+    array.push(coordinates);
+  }
+}
+
+function bottom() {
+  for (let i = 0; i < regularHex.length - 1; i++) {
+    let coords1 = regularHex[i];
+    let coords2 = regularHex[i + 1];
+    let coords3 = smallerHex[i + 1];
+    let coords4 = smallerHex[i];
+    // end of the pencil
+    push();
+    fill(244, 226, 198);
+    noStroke();
+    beginShape();
+    vertex(coords1.x, coords1.y, coords1.z);
+    vertex(coords2.x, coords2.y, coords2.z);
+    vertex(coords3.x, coords3.y, coords3.z);
+    vertex(coords4.x, coords4.y, coords4.z);
+    endShape(CLOSE);
+    pop();
+    // graphite tip
+    push();
+    fill(32);
+    noStroke();
+    beginShape();
+    vertex(coords3.x, coords3.y, coords3.z);
+    vertex(coords4.x, coords4.y, coords4.z);
+    vertex(0, 0, -54);
+    endShape(CLOSE);
+    pop();
+  }
+}
+
+function middle() {
+  for (let i = 0; i < pencilBody.length - 7; i++) {
+    let coords1 = pencilBody[i];
+    let coords2 = pencilBody[i + 1];
+    let coords3 = pencilBody[i + 7];
+    let coords4 = pencilBody[i + 6];
+    if (i < 56) {
+      // body of pencil ?
+      push();
+      fill(237, 145, 33);
+      noStroke();
+      beginShape();
+      vertex(coords1.x, coords1.y, coords1.z);
+      vertex(coords2.x, coords2.y, coords2.z);
+      vertex(coords3.x, coords3.y, coords3.z);
+      vertex(coords4.x, coords4.y, coords4.z);
+      endShape(CLOSE);
+      pop();
+    }
+    else {
+      // metal part
+      push();
+      fill(192);
+      noStroke();
+      beginShape();
+      vertex(coords1.x, coords1.y, coords1.z);
+      vertex(coords2.x, coords2.y, coords2.z);
+      vertex(coords3.x, coords3.y, coords3.z);
+      vertex(coords4.x, coords4.y, coords4.z);
+      endShape(CLOSE);
+      pop();
+    }
+  }
+}
+
+function eraser() {
+  push();
+  noStroke();
+  fill(255, 182, 193);
+  translate(0, 0, 290);
+  sphere(15);
+  pop();
+}
+
+function unravel() {
 
 }
